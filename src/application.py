88f35cd8c9b5c77b32c2fc3c9fcf09af1f4398aa
Manuel Cortéz <manuel@manuelcortez.net>
2016-02-13 15:48:30 -0600

    Added application information

---
+++ src/application.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+name = "Socializer"
+snapshot = True
+if snapshot == False:
+	version = "0.1"
+else:
+	version = "0.1"
+author = u"Manuel Cortéz"
+authorEmail = "manuel@manuelcortez.net"
+copyright = u"Copyright (C) 2016, Manuel cortéz."
+description = unicode(name+" Is an accessible VK client for Windows.")
+
+# The short name will be used for detecting translation files. See languageHandler for more details.
+short_name = "socializer"
