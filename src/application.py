ef8f1a86fc4be4095e1525d7de413c1866ce1399
Manuel Cortéz <manuel@manuelcortez.net>
2016-02-21 08:17:12 -0600

    Added setup script

--- src/application.py
+++ src/application.py
@@ -9,6 +9,6 @@ author = u"Manuel Cortéz"
 authorEmail = "manuel@manuelcortez.net"
 copyright = u"Copyright (C) 2016, Manuel cortéz."
 description = unicode(name+" Is an accessible VK client for Windows.")
-
+url = "https://github.com/manuelcortez/socializer"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "socializer"
