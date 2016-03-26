828dda3ecdf2e94e0d96ca51acb94c3afafa8512
Manuel Cortéz <manuel@manuelcortez.net>
2016-03-26 02:30:34 -0600

    Added a script for making nightly builds of socializer

---
+++ scripts/build.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Define paths for a regular use, if there are not paths for python32 or 64, these commands will be taken.
+pythonpath32="/C/python27x86"
+
+help () {
+	echo -e "$0 | usage:"
+	echo -e "$0 | \t./build.sh [-py32path <path to python for 32 bits> | -h]"
+}
+
+# parsing options from the command line
+while [[ $# > 1 ]]
+	do
+	key="$1"
+	shift
+
+case $key in
+	-py32path)
+		pythonpath32="$1"
+		shift
+	;;
+		-help)
+		help
+	;;
+	*)
+	help
+esac
+done
+
+cd ../src
+if [ -d build/ ];
+	then
+	rm -rf build
+fi
+if [ -d dist/ ];
+	then
+	rm -rf dist
+fi
+$pythonpath32/python.exe "setup.py" "py2exe" "--quiet"
+mv -f dist ../nightly/socializer
+rm -rf build
+cd ../nightly
+$pythonpath32/python.exe make_zipversion.py
