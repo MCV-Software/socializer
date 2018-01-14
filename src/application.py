0e83c7368db2fb331363f49fa81d31905eafd4f4
Manuel Cortez <manuel@manuelcortez.net>
2018-01-14 07:22:54 -0600

    Updated author information

--- src/application.py
+++ src/application.py
@@ -3,9 +3,9 @@ name = "Socializer"
 version = "0.15"
 author = u"Manuel Cortéz"
 authorEmail = "manuel@manuelcortez.net"
-copyright = u"Copyright (C) 2016-2017, MCC Technologies"
+copyright = u"Copyright (C) 2016-2018, Manuel Cortez"
 description = unicode(name+" Is an accessible VK client for Windows.")
-url = "https://github.com/manuelcortez/socializer"
+url = "https://manuelcortez.net/socializer"
 update_url = "https://raw.githubusercontent.com/manuelcortez/socializer/master/update-files/socializer.json"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "socializer"
