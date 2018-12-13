d79f29d237979566e2d5fc8685275b19b3abebc3
Manuel Cortez <manuel@manuelcortez.net>
2018-12-13 17:14:14 -0600

    Update URL in app info

--- src/application.py
+++ src/application.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 name = "Socializer"
-version = "0.15"
-author = u"Manuel Cortéz"
+version = "0.16"
+author = u"Manuel Cortez"
 authorEmail = "manuel@manuelcortez.net"
 copyright = u"Copyright (C) 2016-2018, Manuel Cortez"
 description = unicode(name+" Is an accessible VK client for Windows.")
 url = "https://manuelcortez.net/socializer"
-update_url = "https://raw.githubusercontent.com/manuelcortez/socializer/master/update-files/socializer.json"
+update_url = "https://code.manuelcortez.net/manuelcortez/socializer/raw/master/update-files/socializer.json"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "socializer"
 translators = [u"Valeria K (Russian)", u"Manuel Cortez (Spanish)"]
