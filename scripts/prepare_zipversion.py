a93d334b1f3112173e9c11c6c95f750a022c60b2
Manuel Cortez <manuel@manuelcortez.net>
2019-01-01 20:56:12 -0600

    Attempt to update CI to Python 3. #16

--- scripts/prepare_zipversion.py
+++ scripts/prepare_zipversion.py
@@ -1,13 +1,18 @@
 #! /usr/bin/env python# -*- coding: iso-8859-1 -*-
 import  shutil
 import os
+import sys
 
 def create_archive():
 	os.chdir("..\\src")
-	print "Creating zip archive..."
-	shutil.make_archive("socializer", "zip", "dist")
-	if os.path.exists("dist"):
-		shutil.rmtree("dist")
+	print("Creating zip archive...")
+	if sys.version[0] == "3":
+		folder = "dist/main"
+	else:
+		folder = "dist"
+	shutil.make_archive("socializer", "zip", folder)
+#	if os.path.exists("dist"):
+#		shutil.rmtree("dist")
 	if os.path.exists("build"):
 		shutil.rmtree("build")
 	os.chdir("..\\scripts")
