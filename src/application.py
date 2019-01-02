59803d547c3579e9f70a771979b72c035e764bc3
Manuel Cortez <manuel@manuelcortez.net>
2019-01-01 20:19:39 -0600

    More bugfixes for Python 2

--- src/application.py
+++ src/application.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 name = "Socializer"
 version = "0.17"
--- src/setup.py
+++ src/setup.py
@@ -17,7 +17,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ############################################################
-from __future__ import unicode_literals
 from setuptools import setup, find_packages
 import py2exe
 import os
