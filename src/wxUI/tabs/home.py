eea264a099db8c376c46e1ea44dda247a6928070
Manuel Cortez <manuel@manuelcortez.net>
2019-01-31 16:47:27 -0600

    Added topic buffer's GUI

--- src/wxUI/tabs/home.py
+++ src/wxUI/tabs/home.py
@@ -136,6 +136,17 @@ class friendsTab(homeTab):
 		self.list.set_windows_size(0, 400)
 		self.list.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnKeyDown)
 
+class topicTab(homeTab):
+	def create_list(self):
+		self.lbl = wx.StaticText(self, wx.NewId(), _("Topics"))
+		self.list = widgetUtils.list(self, *[_("User"), _("Title"), _("Posts"), _("Last")], style=wx.LC_REPORT)
+		self.list.set_windows_size(0, 200)
+		self.list.set_windows_size(1, 64)
+		self.list.set_windows_size(2, 15)
+		self.list.set_windows_size(2, 250)
+		self.list.set_size()
+		self.list.list.Bind(wx.EVT_LIST_ITEM_ACTIVATED, self.OnKeyDown)
+
 class empty(wx.Panel):
 	def __init__(self, parent, name):
 		super(empty, self).__init__(parent=parent, name=name)
