e0a3b6db70caa620d8cae707a9d6b1131eeaa322
Manuel Cortez <manuel@manuelcortez.net>
2019-11-19 17:41:09 -0600

    Created a version that would zip the version generated with cx_freeze

---
+++ scripts/prepare_zipversion_cxfreeze.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python# -*- coding: iso-8859-1 -*-
+import  shutil
+import os
+import sys
+
+def create_archive():
+	os.chdir("..\\src")
+	print("Creating zip archive...")
+	folder = "dist"
+	shutil.make_archive("socializer", "zip", folder)
+#	if os.path.exists("dist"):
+#		shutil.rmtree("dist")
+	if os.path.exists("build"):
+		shutil.rmtree("build")
+	os.chdir("..\\scripts")
+
+create_archive()
