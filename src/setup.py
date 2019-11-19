c860834f1743b89e910fc46e25bfa378ab686489
Manuel Cortez <manuel@manuelcortez.net>
2019-11-19 17:33:56 -0600

    Remove unneeded code from the experimental setup file

--- src/setup.py
+++ src/setup.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 import sys
 import application
-from cx_Freeze import setup, Executable
 import platform
 import os
-from glob import glob
+from cx_Freeze import setup, Executable
 
 def find_sound_lib_datafiles():
 	import os
@@ -26,15 +25,6 @@ def find_accessible_output2_datafiles():
 	dest_dir = os.path.join('accessible_output2', 'lib')
 	return (path, dest_dir)
 
-def get_data():
-    """ Get data files for the project. """
-    import accessible_output2
-    import sound_lib
-    datas = [
-        (["session.defaults", "app-configuration.defaults", "cacert.pem"], ""),]+get_sounds()+get_locales()+get_documentation()+get_architecture_files()
-    print(datas)
-    return datas
-
 base = None
 if sys.platform == 'win32':
     base = 'Win32GUI'
@@ -44,7 +34,6 @@ build_exe_options = dict(
 	optimize=2,
 	include_msvcr=True,
 	zip_include_packages=["accessible_output2", "sound_lib", "arrow"],
-#	zip_exclude_packages=["lxml", "wx", "sound_lib", "enchant", "accessible_output2"],
 	include_files=["session.defaults", "cacert.pem", "app-configuration.defaults", "locales", "sounds", "documentation", "../windows-dependencies/x86/oggenc2.exe", "../windows-dependencies/x86/bootstrap.exe", ("../windows-dependencies/dictionaries", "lib/enchant/share/enchant/myspell"), find_sound_lib_datafiles(), find_accessible_output2_datafiles()],
 	packages=["interactors", "presenters", "views", "wxUI"],
 	)
