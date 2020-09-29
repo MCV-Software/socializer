90d2aa75197de8335e57d2913566c6d0e693c2f1
Manuel Cortez <manuel@manuelcortez.net>
2020-09-29 13:30:34 -0500

    Modified translator stuff for docs

--- doc/translate.py
+++ doc/translate.py
@@ -1,5 +1,5 @@
 from babel.messages.frontend import CommandLineInterface
 
-CommandLineInterface().run(['pybabel', 'extract', '-o', 'socializer-documentation.pot', '.'])
+CommandLineInterface().run(['pybabel', 'extract', '-o', '..\scripts\socializer-documentation.pot', '.'])
 #CommandLineInterface().run(['pybabel','compile','-d','../src/locales'])
 CommandLineInterface().run(['pybabel','update', '--input-file', 'socializer-documentation.pot', '--domain', 'socializer-documentation', '--output-dir', '../src/locales'])
