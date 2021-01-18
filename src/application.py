93825971088992600efc665122590fac553c781b
Manuel Cortez <manuel@manuelcortez.net>
2021-01-18 15:49:30 -0600

    Updated internal URLS to make reference to files

--- src/application.py
+++ src/application.py
@@ -14,9 +14,9 @@ bts_access_token = "U29jaWFsaXplcg"
 bts_url = "https://issues.manuelcortez.net"
 ### Update information
 # URL to retrieve the latest updates for the stable branch.
-update_stable_url = "http://socializer.su/static/files/update/stable.json"
+update_stable_url = "https://files.manuelcortez.net/socializer/update/stable.json"
 # URL to retrieve update information for the "next" branch. This is a channel made for alpha versions.
 # Every commit will trigger an update, so users wanting to have the bleeding edge code will get it as soon as it is committed here and build by a runner.
-update_next_url = "http://socializer.su/static/files/update/alpha.json"
+update_next_url = "https://files.manuelcortez.net/socializer/update/alpha.json"
 # Short_id of the last commit, this is set to none here because it will be set manually by the building tools.
 update_next_version = "03286a44"
