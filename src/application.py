211673a8216cb6d84f376bd949dd5fc798f8a225
Manuel Cortez <manuel@manuelcortez.net>
2021-12-31 18:35:26 -0600

    Updated about information

--- src/application.py
+++ src/application.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 name = "Socializer"
-author = "Manuel Cortez"
-authorEmail = "manuel@manuelcortez.net"
-copyright = "Copyright (C) 2016-2020, Manuel Cortez"
+author = "MCV Software"
+authorEmail = "info@mcvsoftware.com"
+copyright = "Copyright (C) 2016-2022, MCV Software"
 description = name+" Is an accessible VK client for Windows."
-url = "http://socializer.su"
+url = "https://socializer.su"
 # The short name will be used for detecting translation files. See languageHandler for more details.
 short_name = "socializer"
 translators = ["Darya Ratnikova (Russian)", "Manuel Cortez (Spanish)"]
@@ -12,7 +12,7 @@ bts_name = "socializer"
 bts_access_token = "U29jaWFsaXplcg"
 bts_url = "https://issues.manuelcortez.net"
 ### Update information
-update_url = "https://files.manuelcortez.net/socializer/update/alpha.json"
+update_url = "https://files.mcvsoftware.com/socializer/update/alpha.json"
 # Short_id of the last commit, this is set to none here because it will be set manually by the building tools.
 version = "2021.09.22"
 update_next_version = "4bdba42e"
